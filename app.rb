6b9b0b490c90ded5bed782a3c50fef5b861e51bb
DemetriousWilson <DemetriousWilson@gmail.com>
2013-02-19 03:57:36 -0800

    Added url autocomplete

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'json'
-require 'net/https'
 require 'openssl'
 require 'nokogiri'
 require 'open-uri'
@@ -10,12 +9,50 @@ set server: 'thin'
 
 class Scrape
   def self.images(uri_string)
+    uri_string = self.fix_url(uri_string)
+    raise Exception if uri_string == :fix_url_failed
     raw_img_tags = Nokogiri::HTML(open(uri_string, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)).css('img')
     img_array = []
     0.upto(raw_img_tags.size - 1) do |index|
       img_array.push index, raw_img_tags[index].to_s
     end
     img_hash = Hash[*img_array]
+  end
+
+private
+
+  def self.ping?(url)
+    uri = URI(url)
+    begin
+      Nokogiri::HTML(open(uri.to_s, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)).css('title')
+      true
+    rescue Exception => exception
+      false
+    end
+  end
+
+  def self.try_url(url, option)
+    case option
+      when 1
+        return "http://" + url
+      when 2
+        return "http://www." + url
+      when 3
+        return "https://" + url
+      when 4
+        return "https://www." + url
+      else
+        return url
+    end
+  end
+
+  def self.fix_url(url)
+    0.upto(4) do |n|
+      if self.ping?(self.try_url(url, n))
+        return self.try_url(url, n)
+      end
+    end
+    :fix_url_failed
   end
 end
 
@@ -27,12 +64,7 @@ post '/validate' do
   url = params["url"]
   begin
     uri = URI(url.to_s)
-    code = Net::HTTP.get_response(uri).code
-    if code == "200"
-      Scrape.images(uri.to_s).to_json()
-    else
-      {:error => "error", :code => code}.to_json()
-    end
+    Scrape.images(uri.to_s).to_json()
   rescue Exception => exception
     puts "#{exception.class}: #{exception.message}"
     {:error => "error", :code => "invalid"}.to_json()
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -7,18 +7,25 @@ $(document).ready( function() {
         callback();
     };
 
-    var updateTip = function(code) {
-        var message = {
-            '404': "That url was not found!",
-            '502': "There was a server side error on that page!"
-        };
-
-        setTimeout(
-            function () {
-                $('#urlInput').poshytip('update', message[code]);
-            }, 2000
-        );
+    var spinnerOptions = {
+        lines: 13, // The number of lines to draw
+        length: 7, // The length of each line
+        width: 4, // The line thickness
+        radius: 10, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        color: '#000', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
     };
+    var spinnerTarget = document.getElementById('spinnerDiv');
+    var spinner = new Spinner(spinnerOptions);
 
 	$('#urlInput').poshytip({
 		content: 'Invalid Url please try again!',
@@ -33,11 +40,13 @@ $(document).ready( function() {
 	$('#mainForm').submit(function(event) {
 		event.preventDefault();
 		var inputUrl = $('#urlInput').val();
+        spinner.spin(spinnerTarget);
 
 		$.post('/validate', {
 			url: inputUrl
 		}, function(callbackData) {
 			var dataParsed = $.parseJSON(callbackData);
+            spinner.stop();
 
 			if(dataParsed.error !== undefined) { //Handle error
                 console.log("code: " + dataParsed.code); // Debug
